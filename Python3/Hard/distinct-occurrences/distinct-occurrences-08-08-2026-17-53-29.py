792037d563af6010f6cf1e4a9c0ff699a2cdd6bd
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-08 17:53:31 +0530

    Amazon | Hard | distinct-occurrencesPython3 | 0.05s | Unknown 08-08-2026

---
+++ Python3/Hard/distinct-occurrences/distinct-occurrences-08-08-2026-17-53-29.py
@@ -0,0 +1,30 @@
+class Solution:
+    def subseqCount(self, txt, pat):
+        s = txt
+        t = pat
+
+        memo = [[-1 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
+
+        def solve(i, j, count):
+            if i >= len(s) or j >= len(t):
+                if count == len(t):
+                    memo[i][j] = 1
+                    return 1
+                memo[i][j] = 0
+                return 0
+
+            if memo[i][j] != -1:
+                return memo[i][j]
+
+            take = 0
+            # take
+            if s[i] == t[j]:
+                take = solve(i + 1, j + 1, count + 1)
+
+            # skip
+            skip = solve(i + 1, j, count)
+
+            memo[i][j] = take + skip
+            return memo[i][j]
+
+        return solve(0, 0, 0)
