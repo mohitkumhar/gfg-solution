ea63f119d47efb29784944bc8de58e3dd57d76be
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-14 22:35:53 +0530

    Amazon | Medium | single-among-doubles-in-a-sortedPython3 | 0.3s | Unknown 08-14-2026

---
+++ Python3/Medium/single-among-doubles-in-a-sorted/single-among-doubles-in-a-sorted-08-14-2026-22-35-50.py
@@ -0,0 +1,26 @@
+class Solution:
+    def single(self, arr):
+        nums = arr
+
+        n = len(nums)
+
+        left = 1
+        right = n - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == nums[mid + 1]:
+                if (right - mid) % 2 == 0:
+                    left = mid + 2
+                else:
+                    right = mid - 1
+
+            else:
+                if (right - mid) % 2 == 0:
+                    right = mid
+
+                else:
+                    left = mid + 1
+
+        return nums[right]
