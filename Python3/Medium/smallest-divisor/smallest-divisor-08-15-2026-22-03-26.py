a7e3f0f32c06c4bc6302f3236650ada86ea4caa7
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-15 22:03:28 +0530

    Binary Search | Medium | smallest-divisorPython3 | 2.78s | Unknown 08-15-2026

---
+++ Python3/Medium/smallest-divisor/smallest-divisor-08-15-2026-22-03-26.py
@@ -0,0 +1,33 @@
+class Solution:
+    def smallestDivisor(self, nums, threshold):
+
+        def isPossible(val, nums, threshold):
+
+            count = 0
+
+            for num in nums:
+                flag = 0
+
+                if num % val != 0:
+                    flag = 1
+
+                count += (num // val) + flag
+
+            return count <= threshold
+
+
+        n = len(nums)
+
+        left = 1
+        right = max(nums)
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isPossible(mid, nums, threshold):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+
