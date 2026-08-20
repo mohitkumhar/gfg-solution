bce8da55a1d847c274b9e2e72f26946dc6029809
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-20 19:51:06 +0530

    Microsoft | Hard | 0the-painters-partition-problem-iiC++ | 0.47s | Unknown 08-20-2026

---
+++ C++/Hard/0the-painters-partition-problem-ii/0the-painters-partition-problem-ii-08-20-2026-19-50-58.cpp
@@ -0,0 +1,43 @@
+class Solution {
+	public:
+	bool isPossible(int maxVal, vector<int> nums, int k)
+	{
+		int currVal = 0;
+		int currPainter = 1;
+		
+		for (int num : nums)
+			{
+			if ((currVal + num) > maxVal)
+				{
+				currPainter++;
+				currVal = num;
+			}
+			else
+				currVal += num;
+		}
+		return currPainter <= k;
+	}
+	
+	int minTime(vector<int>& nums, int k) {
+		
+		int left = *max_element(nums.begin(), nums.end());
+		int right = accumulate(nums.begin(), nums.end(), 0);
+		
+		int ans = 0;
+		
+		while (left <= right)
+			{
+			int mid = left + (right - left) / 2;
+			
+			if (isPossible(mid, nums, k))
+				{
+				ans = mid;
+				right = mid - 1;
+			}
+			else
+				left = mid + 1;
+		}
+		return ans;
+		
+	}
+};
