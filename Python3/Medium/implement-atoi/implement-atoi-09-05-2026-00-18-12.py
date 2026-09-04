d18be3d16c82884ed8013ce1db08816c54799e4e
Mohit Kumhar <mohitmolela@gmail.com>
2026-09-05 00:18:15 +0530

    Morgan Stanley | Medium | implement-atoi.Python3 | 0.03s | Unknown

---
+++ Python3/Medium/implement-atoi/implement-atoi-09-05-2026-00-18-12.py
@@ -0,0 +1,28 @@
+class Solution:
+    def myAtoi(self, s):
+        # code here
+        n = len(s)
+        sign = 1
+        i = 0
+        result = 0
+
+        while i < n and s[i] == " ":
+            i += 1
+
+        if i < n and (s[i] == "-" or s[i] == "+"):
+            if s[i] == "-":
+                sign = -1
+            i += 1
+
+        while i < n and s[i].isdigit():
+            result = result * 10 + int(s[i])
+            i += 1
+
+        result *= sign
+
+        if result < -2 ** 31:
+            return - 2 ** 31
+        if result > 2 ** 31 - 1:
+            return 2 ** 31 - 1
+
+        return result
