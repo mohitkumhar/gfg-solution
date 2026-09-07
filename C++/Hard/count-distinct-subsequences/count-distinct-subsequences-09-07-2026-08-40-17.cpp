19cb16aa72b5387b5cca4af698e642e787c84926
Mohit Kumhar <mohitmolela@gmail.com>
2026-09-07 08:40:20 +0530

    Strings | Hard | count-distinct-subsequences.C++ | 0.21s | Unknown

---
+++ C++/Hard/count-distinct-subsequences/count-distinct-subsequences-09-07-2026-08-40-17.cpp
@@ -0,0 +1,15 @@
+class Solution {
+	public:
+	int distinctSubseq(string &s) {
+		// code here
+		long long MOD = 1e9 + 7;
+		vector<long long> dp(26, 0);
+		
+		for (char ch : s) {
+			int idx = ch - 'a';
+			dp[idx] = (accumulate(dp.begin(), dp.end(), 0LL) + 1) % MOD;
+		}
+		return (accumulate(dp.begin(), dp.end(), 0LL)) % MOD + 1;
+	}
+};
+
